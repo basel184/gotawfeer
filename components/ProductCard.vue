8c074b71fde9cbfa2bcd574b09c15d41dfca5197
HossaamHassan <hossam.h.eldeeb@gmail.com>
2025-11-04 18:00:23 +0200

    fix tooltip

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import { computed, ref, watchEffect, onMounted } from 'vue'
+import { computed, ref, watchEffect, onMounted, nextTick } from 'vue'
 import { useBrands } from '../composables/useBrands'
 import { useWishlist } from '../composables/useWishlist'
 import { useCart } from '../composables/useCart'
 import { useCompare } from '../composables/useCompare'
+
+
 const props = defineProps<{ product: any; qty?: number; busy?: boolean }>()
 const emit = defineEmits<{
   (e: 'add', product: any): void;
@@ -256,7 +258,11 @@ onMounted(async () => {
   } catch (error) {
     console.error('Failed to load cart or wishlist:', error)
   }
+
+
 })
+
+
 
 await ensureBrands()
 // Ensure specific brand is loaded if only brand_id provided
@@ -712,6 +718,9 @@ const openProductModal = (e: Event) => {
           :data-wished="wished"
           :data-loading="wishlistLoading"
           aria-label="Wishlist"
+          data-bs-toggle="tooltip" data-bs-placement="top"
+          data-bs-custom-class="custom-tooltip"
+          data-bs-title="المفضلة"
           >
           <svg v-if="!wishlistLoading" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M10 1C11.6566 1 13 2.49112 13 4.33081C13 5.81587 12.475 9.34047 7.3072 12.9034C7.21463 12.9666 7.10836 13 7 13C6.89164 13 6.78537 12.9666 6.6928 12.9034C1.525 9.34047 1 5.81587 1 4.33081C1 2.49112 2.3434 1 4 1C5.6566 1 7 3.01867 7 3.01867C7 3.01867 8.3434 1 10 1Z" stroke="black" stroke-opacity="0.49" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
