5d81c1653cebefa8efdb8ce0958e13497260bbb3
Basel Atef <84516452+basel184@users.noreply.github.com>
2025-12-14 18:00:24 +0200

    سسسصضضصضص

--- pages/cart/index.vue
+++ pages/cart/index.vue
@@ -726,24 +726,6 @@ function hasDiscount(it: any): boolean {
                       </svg>
                       {{ it?.shop?.name }}
                     </span>
-                    <span v-if="it?.variant" class="variant">
-                      <svg width="12" height="12" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M5.5 7A1.5 1.5 0 0 1 4 5.5A1.5 1.5 0 0 1 5.5 4A1.5 1.5 0 0 1 7 5.5A1.5 1.5 0 0 1 5.5 7m15 0A1.5 1.5 0 0 1 19 5.5A1.5 1.5 0 0 1 20.5 4A1.5 1.5 0 0 1 22 5.5A1.5 1.5 0 0 1 20.5 7m-7 0A1.5 1.5 0 0 1 12 5.5A1.5 1.5 0 0 1 13.5 4A1.5 1.5 0 0 1 15 5.5A1.5 1.5 0 0 1 13.5 7M5.5 13A1.5 1.5 0 0 1 4 11.5A1.5 1.5 0 0 1 5.5 10A1.5 1.5 0 0 1 7 11.5A1.5 1.5 0 0 1 5.5 13m7 0A1.5 1.5 0 0 1 12 11.5A1.5 1.5 0 0 1 13.5 10A1.5 1.5 0 0 1 15 11.5A1.5 1.5 0 0 1 13.5 13m7 0A1.5 1.5 0 0 1 19 11.5A1.5 1.5 0 0 1 20.5 10A1.5 1.5 0 0 1 22 11.5A1.5 1.5 0 0 1 20.5 13M5.5 19A1.5 1.5 0 0 1 4 17.5A1.5 1.5 0 0 1 5.5 16A1.5 1.5 0 0 1 7 17.5A1.5 1.5 0 0 1 5.5 19m7 0A1.5 1.5 0 0 1 12 17.5A1.5 1.5 0 0 1 13.5 16A1.5 1.5 0 0 1 15 17.5A1.5 1.5 0 0 1 13.5 19m7 0A1.5 1.5 0 0 1 19 17.5A1.5 1.5 0 0 1 20.5 16A1.5 1.5 0 0 1 22 17.5A1.5 1.5 0 0 1 20.5 19Z"/>
-                      </svg>
-                      {{ it?.variant }}
-                    </span>
-                    <span v-if="it?.variant_type" class="variant-type">
-                      <svg width="12" height="12" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/>
-                      </svg>
-                      {{ it?.variant_type }}
-                    </span>
-                    <span v-if="it?.sku" class="sku">
-                      <svg width="12" height="12" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8l-6-6z"/>
-                      </svg>
-                      {{ it?.sku }}
-                    </span>
                     <span v-if="it?.color" class="color-info">
                       <svg width="12" height="12" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm-2 15l-5-5 1.41-1.41L10 14.17l7.59-7.59L19 8l-9 9z"/>
