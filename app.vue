7ca8a3e9282fe096870bee595dd5de20b1233a38
Basel Atef <84516452+basel184@users.noreply.github.com>
2026-01-09 15:18:14 +0200

    Add Snapchat Pixel tracking script to app head configuration
    - Add Snapchat Pixel initialization script with ID 'f607062b-c823-407a-9f93-1dc2542be238'
    - Add PAGE_VIEW tracking event
    - Disable sanitizers for script tag to allow inline JavaScript execution

--- app.vue
+++ app.vue
@@ -56,7 +56,14 @@ useHead(() => {
     },
     meta: [
       { name: 'facebook-domain-verification', content: '1m0v9vhvrgpr5kj8cdegfh5wttfm1x' }
-    ]
+    ],
+    script: [
+      {
+        innerHTML: `(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function(){a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};a.queue=[];var s='script';var r=t.createElement(s);r.async=!0;r.src=n;var u=t.getElementsByTagName(s)[0];u.parentNode.insertBefore(r,u);})(window,document,'https://sc-static.net/scevent.min.js');snaptr('init','f607062b-c823-407a-9f93-1dc2542be238',{});snaptr('track','PAGE_VIEW');`,
+        type: 'text/javascript'
+      }
+    ],
+    __dangerouslyDisableSanitizers: ['script']
   }
 })
 
